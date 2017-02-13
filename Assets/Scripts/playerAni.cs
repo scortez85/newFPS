ae99e63010bc7c8298fdbb8c779091534076c8d9
Steven Cortez <steven_cortez@msn.com>
2017-02-12 21:06:14 -0600

    added character

---
+++ Assets/Scripts/playerAni.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerAni : MonoBehaviour {
+
+    Animator ani;
+    hashID hashIDs;
+	void Start () {
+        ani = GetComponent<Animator>();
+        hashIDs = GetComponent<hashID>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vert = Input.GetAxis("Vertical");
+        float horiz = Input.GetAxis("Horizontal");
+
+        if (vert != 0 || horiz != 0)
+        {
+            ani.SetFloat(hashIDs.speed, 5);
+        }
+        else ani.SetFloat(hashIDs.speed, 0);
+	
+	}
+}
